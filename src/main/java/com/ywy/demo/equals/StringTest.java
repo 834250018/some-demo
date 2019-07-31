5e325142aadd61306634e73dc5e1524619b63d3e
weiye <834250018@qq.com>
2019-07-31 12:37:59 +0800

    1.StringTest的一个认知错误,String类型入参为地址传递,但修改变量时也是修改地址,所以方法内改变一个变量,方法外变量的值不变

--- src/main/java/com/ywy/demo/equals/StringTest.java
+++ src/main/java/com/ywy/demo/equals/StringTest.java
@@ -9,9 +9,10 @@ import com.ywy.demo.design_pattern.builder_pattern.User;
 public class StringTest {
     public static void main(String[] args) {
         String str = "aaa";
-        // 字符串值传递
+        System.out.println("执行方法之前的str:" + str); // aaa
+        // 字符串地址传递,基本数据类型才是值传递
         changeStr(str);
-        System.out.println(str); // aaa
+        System.out.println("执行方法之后的str:" + str); // aaa
 
         String str1 = "aaab".replace("b", "");
         System.out.println(str == str1); // false
@@ -22,6 +23,6 @@ public class StringTest {
     }
 
     static void changeStr(String str) {
-        str = "bbb";
+        str = "bbb"; // 指向新地址,不影响源地址,相当于str = new String("bbb");
     }
 }
