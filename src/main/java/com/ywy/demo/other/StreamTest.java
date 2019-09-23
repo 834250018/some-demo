d38741d7904f7b7b7bd825cdd87125f4c4bd077d
ve <834250018@qq.com>
2019-09-23 11:51:10 +0800

    1.增加一个jdk8流排序的demo

---
+++ src/main/java/com/ywy/demo/other/StreamTest.java
@@ -0,0 +1,25 @@
+package com.ywy.demo.other;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author ve
+ * @date 2019/9/23 11:30
+ */
+public class StreamTest {
+    public static void main(String[] args) {
+        // 原数组(可以扩展为对象数组中的所有对象的某个字段)
+        List<String> list = Stream.of("dfdsfjs", "uittyut", "nbvnvn", "gfhfghfg", "qweqeq").collect(Collectors.toList());
+        // 排序规则,按这个列表的字符串顺序排序
+        List<String> sortList = Stream.of("uittyut", "qweqeq", "dfdsfjs", "gfhfghfg", "nbvnvn").collect(Collectors.toList());
+        // 权重排序
+        List<String> result = list.stream()
+                // 即.sorted((o1, o2) -> sortList.indexOf(o1) - sortList.indexOf(o2))
+                .sorted(Comparator.comparingInt(sortList::indexOf))
+                .collect(Collectors.toList());
+        System.out.println(result.toString());
+    }
+}
