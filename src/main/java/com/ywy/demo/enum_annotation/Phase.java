ba9ff9e2002b6011d6d30bbbe0c5bf7254b7ab52
weiye <834250018@qq.com>
2020-05-21 01:38:45 +0800

    1.遗传算法

--- src/main/java/com/ywy/demo/enum_annotation/Phase.java
+++ src/main/java/com/ywy/demo/enum_annotation/Phase.java
@@ -30,7 +30,7 @@ public enum Phase {
         // Initialize hte phase transition map
         private static final Map<Phase, Map<Phase, Transition>>
                 m = Stream.of(values()).collect(Collectors.groupingBy(
-                (t) -> t.from, // 通过from分组,当前分三组
+                t -> t.from, // 通过from分组,当前分三组
                 () -> new EnumMap<>(Phase.class), // 创建一个EnumMap,key为分组条件,value为下一个参数处理的内容
                 Collectors.toMap( // 下游收集器,比如SOLID组中,收集的初始内容为MELT跟SUBLIME,这里是把它转成map
                         t -> t.to,// map的键
